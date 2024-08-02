508d905134741a167bc26e20c677f3051b0741a5
Thong Ta <thanthong.ta@gmail.com>
2024-08-02 16:32:12 +0200

    added apollo client, framer motion and link to root

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { motion } from 'framer-motion';
+import {
+	ApolloClient,
+	InMemoryCache,
+	ApolloProvider,
+	gql,
+} from '@apollo/client';
+import { Button } from 'react-aria-components';
+
+const client = new ApolloClient({
+	uri: 'https://rickandmortyapi.com/graphql/',
+	cache: new InMemoryCache(),
+});
+
+client
+	.query({
+		query: gql`
+			query {
+				characters(page: 2, filter: { name: "rick" }) {
+					info {
+						count
+					}
+					results {
+						name
+					}
+				}
+				location(id: 1) {
+					id
+				}
+				episodesByIds(ids: [1, 2]) {
+					id
+				}
+			}
+		`,
+	})
+	.then((result) => console.log(result));
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<h1>React-Projekt 👩🏻‍💻</h1>
+		<ApolloProvider client={client}>
+			<h1>React-Projekt 👩🏻‍💻</h1>
+			<motion.div animate={{ x: 100 }}>Test</motion.div>
+			<Button>Aria Button</Button>
+		</ApolloProvider>
 	</React.StrictMode>
 );
