7a929456eadc92ef176ff5ca3ee6f84d1448239d
Thong Ta <thanthong.ta@gmail.com>
2024-08-03 07:57:45 +0200

    added styling for the categories

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,14 @@ import {
 	ApolloProvider,
 	gql,
 } from '@apollo/client';
-import { Heading, Input, SearchField } from 'react-aria-components';
+import {
+	Heading,
+	Input,
+	Label,
+	Radio,
+	RadioGroup,
+	SearchField,
+} from 'react-aria-components';
 
 const client = new ApolloClient({
 	uri: 'https://rickandmortyapi.com/graphql/',
@@ -41,28 +48,30 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<ApolloProvider client={client}>
 			<Heading level="1" className="heading">
-				Rick and Morty React Client
+				Rick and Morty DB
 			</Heading>
-			<nav aria-label="Navigation" className="navigation">
-				<button aria-label="Characters" className="nav__button nav-chars">
-					Characters
-				</button>
-				<button aria-label="Locations" className="nav__button nav-locs">
-					Locations
-				</button>
-				<button aria-label="Episodes" className="nav__button nav-eps">
-					Episodes
-				</button>
-			</nav>
-			<SearchField aria-label="Search form" className="search-form">
-				<Input
-					aria-label="Search"
-					className="search-input"
-					placeholder="Search"
-				/>
-				<motion.button aria-label="Search button" className="search">
-					Search
-				</motion.button>
+
+			<SearchField className="search-form">
+				<RadioGroup className="search-categories" defaultValue={'characters'}>
+					<Label className="search-categories__label">Search Categories:</Label>
+					<Radio value="characters" className="search-categories__radio">
+						Characters
+					</Radio>
+					<Radio value="locations" className="search-categories__radio">
+						Locations
+					</Radio>
+					<Radio value="episodes" className="search-categories__radio">
+						Episodes
+					</Radio>
+				</RadioGroup>
+				<section className="search-inputs">
+					<Label className="visually-hidden">Search</Label>
+					<Input className="search-input" placeholder="Search" />
+					<motion.button className="search button--ghost">Search</motion.button>
+					<motion.button className="show-all button--ghost">
+						Show all
+					</motion.button>
+				</section>
 			</SearchField>
 			<section className="search-results">
 				<section className="search-results__list"></section>
